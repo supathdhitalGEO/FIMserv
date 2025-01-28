095fa5931fb13192b3ed01b73d5933bbac3c450b
supathdhitalGEO <sdhital@crimson.ua.edu>
2025-01-28 07:26:31 -0600

    minor improvements on the running part

--- src/fimserve/runFIM.py
+++ src/fimserve/runFIM.py
@@ -1,11 +1,11 @@
 import os
 import sys
 import glob
+import shutil
 import subprocess
 from dotenv import load_dotenv
 
 from .datadownload import setup_directories
-
 
 def runfim(code_dir, output_dir, HUC_code, data_dir):
     original_dir = os.getcwd()
@@ -24,9 +24,18 @@ def runfim(code_dir, output_dir, HUC_code, data_dir):
 
         # Get the inundation file path
         discharge_basename = os.path.basename(data_dir).split(".")[0]
+        inundation_dir = os.path.join(HUC_dir, f"{HUC_code}_inundation")
+        temp_dir = os.path.join(inundation_dir, "temp")
+
+        if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)
+        
         inundation_file = os.path.join(
-            HUC_dir, f"{HUC_code}_inundation/{discharge_basename}_inundation.tif"
+            temp_dir, f"{discharge_basename}_inundation.tif"
         )
+        # depth_file = os.path.join(
+        #     temp_dir, f"{discharge_basename}_depth.tif"
+        # )
 
         Command = [
             sys.executable,
@@ -39,6 +48,8 @@ def runfim(code_dir, output_dir, HUC_code, data_dir):
             csv_path,
             "-i",
             inundation_file,
+            # "-d",
+            # depth_file,
         ]
         env = os.environ.copy()
         env["PYTHONPATH"] = f"{src_path}{os.pathsep}{code_dir}"
@@ -51,13 +62,25 @@ def runfim(code_dir, output_dir, HUC_code, data_dir):
             stderr=subprocess.PIPE,
         )
 
-        # Print the output and error (if any)
+          # Print the output and error (if any)
         print(result.stdout.decode())
         if result.stderr:
             print(result.stderr.decode())
+
         # Check if the command was successful
         if result.returncode == 0:
             print(f"Inundation mapping for {HUC_code} completed successfully.")
+
+            # Move the generated files to inundation_dir
+            # if os.path.exists(inundation_file) and os.path.exists(depth_file):
+            if os.path.exists(inundation_file):
+                shutil.move(inundation_file, inundation_dir)
+                # shutil.move(depth_file, inundation_dir)
+
+            # Delete the temp directory
+            if os.path.exists(temp_dir):
+                shutil.rmtree(temp_dir)
+
         else:
             print(f"Failed to complete inundation mapping for {HUC_code}.")
 
